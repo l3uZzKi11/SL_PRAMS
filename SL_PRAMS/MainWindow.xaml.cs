157835ca1c059975469eef76662a2acef89f108e
saxveritas <xcarsner@hotmail.com>
2018-09-23 15:54:00 -0600

    fixed mistakes... lost 8 hours of work... test often and document all steps taken...

--- SL_PRAMS/MainWindow.xaml.cs
+++ SL_PRAMS/MainWindow.xaml.cs
@@ -15,12 +15,15 @@ using System.Windows.Shapes;
 using System.Windows.Forms;
 using Devart.Data.MySql;
 using System.Globalization;
+//using MahApps.Metro.Controls;
+//using MahApps.Metro.Controls.Dialogs;
 
 namespace _01DUMMY_SL_PRAMS
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    //public partial class MainWindow : MetroWindow
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -55,6 +58,7 @@ namespace _01DUMMY_SL_PRAMS
         private string serviceagreement;
         private string fairandreasonable;
         private string approvedsource;
+
 
         private void Submit_Button_Click(object sender, RoutedEventArgs e)
         {
@@ -139,7 +143,7 @@ namespace _01DUMMY_SL_PRAMS
 
             string constring = "host=192.168.182.128;port=3306;User Id=SL_PRAMS_USER;Password=SLPRAMSpassword2018";
             string Command = "INSERT INTO slprams.impact(firstname,middlename,lastname,officesymbol,squadron,date,mandatorysource,requireauthorizations,basesupply,tco,cipswoms,serviceagreement,fairandreasonable,approvedsource,description,justification,vendor1,contract1,url1,cost1,vendor2,contract2,url2,cost2,vendor3,contract3,url3,cost3)VALUES('" + First_name_textbox.Text + "','" + MI_textbox.Text + "','" + LAST_NAME_textbox.Text + "','" + OFFICE_SYMBOL_textbox.Text + "','" + SQUADRON_textbox.Text + "','" + date_selector.Text + "','" + mandatorysource + "','" + requireauthorizations + "','" + basesupply + "','" + tco + "','" + cipswoms + "','" + serviceagreement + "','" + fairandreasonable + "','" + approvedsource + "','" + this.IMPACT_01.Text + "','" + this.IMPACT_03.Text + "','" + this.IMPACT_05.Text + "','" + this.IMPACT_07.Text + "','" + this.IMPACT_09.Text + "','" + this.IMPACT_11.Text + "','" + this.IMPACT_13.Text + "','" + this.IMPACT_15.Text + "','" + this.IMPACT_17.Text + "','" + this.IMPACT_19.Text + "','" + this.IMPACT_21.Text + "','" + this.IMPACT_23.Text + "','" + this.IMPACT_25.Text + "','" + this.IMPACT_27.Text + "')";
-            string Command2 = "INSERT INTO slprams.impact()VALUES()";
+
             MySqlConnection conDataBase = new MySqlConnection(constring);
             MySqlCommand cmdDataBase = new MySqlCommand(Command, conDataBase);
 
@@ -147,8 +151,21 @@ namespace _01DUMMY_SL_PRAMS
 
             try
             {
-                if (String.IsNullOrWhiteSpace(date_selector.Text) || String.IsNullOrWhiteSpace(First_name_textbox.Text) || String.IsNullOrWhiteSpace(MI_textbox.Text) || String.IsNullOrWhiteSpace(LAST_NAME_textbox.Text) || String.IsNullOrWhiteSpace(OFFICE_SYMBOL_textbox.Text) || String.IsNullOrWhiteSpace(SQUADRON_textbox.Text))
+                if (String.IsNullOrWhiteSpace(date_selector.Text) ||
+                    String.IsNullOrWhiteSpace(First_name_textbox.Text) ||
+                    String.IsNullOrWhiteSpace(MI_textbox.Text) ||
+                    String.IsNullOrWhiteSpace(LAST_NAME_textbox.Text) ||
+                    String.IsNullOrWhiteSpace(OFFICE_SYMBOL_textbox.Text) ||
+                    String.IsNullOrWhiteSpace(SQUADRON_textbox.Text))
                 {
+                    /*this.ShowMessageAsync("You've missed mandatory fields on the main tab",
+                        "First name: " + First_name_textbox.Text + "\n" +
+                        "MI: " + MI_textbox.Text + "\n" +
+                        "Last name: " + LAST_NAME_textbox.Text + "\n" +
+                        "Office symbol: " + OFFICE_SYMBOL_textbox.Text + "\n" +
+                        "Squadron: " + SQUADRON_textbox.Text + "\n" +
+                        "Date: " + date_selector.Text + "\n" +
+                        "Please populate all user information fields", MessageDialogStyle.Affirmative);*/
                     System.Windows.Forms.MessageBox.Show("You've missed mandatory fields on the main tab" + "\n" +
                         "First name: " + First_name_textbox.Text + "\n" +
                         "MI: " + MI_textbox.Text + "\n" +
@@ -162,96 +179,101 @@ namespace _01DUMMY_SL_PRAMS
                 {
                     //for debugging
                     //System.Windows.MessageBox.Show(approvedsource);
-                    if (approvedsource == "NO")
+                    if (!String.IsNullOrWhiteSpace(date_selector.Text) &&
+                        !String.IsNullOrWhiteSpace(First_name_textbox.Text) &&
+                        !String.IsNullOrWhiteSpace(MI_textbox.Text) &&
+                        !String.IsNullOrWhiteSpace(LAST_NAME_textbox.Text) &&
+                        !String.IsNullOrWhiteSpace(OFFICE_SYMBOL_textbox.Text) &&
+                        !String.IsNullOrWhiteSpace(SQUADRON_textbox.Text))
                     {
-                        System.Windows.MessageBox.Show("debug1");
-                        if (!String.IsNullOrWhiteSpace(IMPACT_01.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_03.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_05.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_07.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_09.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_11.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_13.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_15.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_17.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_19.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_21.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_23.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_25.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_27.Text))
-                        {
-                            conDataBase.Open();
-                            myReader = cmdDataBase.ExecuteReader();
-                            System.Windows.MessageBox.Show("Purchase Request Submitted!");
-                            while (myReader.Read())
-                            {
-                            }
-                        }
-                        System.Windows.MessageBox.Show("debug2");
-                        if (String.IsNullOrWhiteSpace(IMPACT_05.Text) ||
-                            String.IsNullOrWhiteSpace(IMPACT_07.Text) ||
-                            String.IsNullOrWhiteSpace(IMPACT_09.Text) ||
-                            String.IsNullOrWhiteSpace(IMPACT_11.Text) ||
-                            String.IsNullOrWhiteSpace(IMPACT_13.Text) ||
-                            String.IsNullOrWhiteSpace(IMPACT_15.Text) ||
-                            String.IsNullOrWhiteSpace(IMPACT_17.Text) ||
-                            String.IsNullOrWhiteSpace(IMPACT_19.Text) ||
-                            String.IsNullOrWhiteSpace(IMPACT_21.Text) ||
-                            String.IsNullOrWhiteSpace(IMPACT_23.Text) ||
-                            String.IsNullOrWhiteSpace(IMPACT_25.Text) ||
-                            String.IsNullOrWhiteSpace(IMPACT_27.Text))
-                        {
-                            System.Windows.MessageBox.Show("You must supply all information for 3 vendors if products aren't from an approved source." +
-                                "/n" +
-                                "You must also include a Description of the purchase being requested and a justification.");
-                        }
-                        
-                        else
-                        {
-                            System.Windows.MessageBox.Show("You should not be seeing this error.");
-                        }
-                    }
-                    //for debugging
-                    //System.Windows.MessageBox.Show(approvedsource);
-                    System.Windows.MessageBox.Show("debug3");
-                    if (approvedsource == "YES")
-                    {
-                        if (String.IsNullOrWhiteSpace(IMPACT_05.Text))
-                        {
-                            System.Windows.MessageBox.Show("Enter the approved vendor name");
-                        }
-                        if (String.IsNullOrWhiteSpace(IMPACT_05.Text) && !String.IsNullOrWhiteSpace(IMPACT_13.Text) || !String.IsNullOrWhiteSpace(IMPACT_21.Text))
-                        {
-                            System.Windows.MessageBox.Show("Approved must be in the correct column");
-                        }
-                        if (String.IsNullOrWhiteSpace(IMPACT_03.Text) || String.IsNullOrWhiteSpace(IMPACT_05.Text) || String.IsNullOrWhiteSpace(IMPACT_07.Text) || String.IsNullOrWhiteSpace(IMPACT_09.Text))
-                        {
-                            System.Windows.MessageBox.Show("You have not entered enough information for the approved vendor");
-                        }
-                        if (!String.IsNullOrWhiteSpace(IMPACT_01.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_03.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_05.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_07.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_09.Text) &&
-                            !String.IsNullOrWhiteSpace(IMPACT_11.Text))
-                        {
 
+                        if (approvedsource == "NO")
+                        {
+                            if (!String.IsNullOrWhiteSpace(IMPACT_01.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_03.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_05.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_07.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_09.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_11.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_13.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_15.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_17.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_19.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_21.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_23.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_25.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_27.Text))
+                            {
+                                conDataBase.Open();
+                                myReader = cmdDataBase.ExecuteReader();
+                                System.Windows.MessageBox.Show("Purchase Request Submitted!");
+                                while (myReader.Read())
+                                {
+                                }
+                            }
+                            if (String.IsNullOrWhiteSpace(IMPACT_05.Text) ||
+                                String.IsNullOrWhiteSpace(IMPACT_07.Text) ||
+                                String.IsNullOrWhiteSpace(IMPACT_09.Text) ||
+                                String.IsNullOrWhiteSpace(IMPACT_11.Text) ||
+                                String.IsNullOrWhiteSpace(IMPACT_13.Text) ||
+                                String.IsNullOrWhiteSpace(IMPACT_15.Text) ||
+                                String.IsNullOrWhiteSpace(IMPACT_17.Text) ||
+                                String.IsNullOrWhiteSpace(IMPACT_19.Text) ||
+                                String.IsNullOrWhiteSpace(IMPACT_21.Text) ||
+                                String.IsNullOrWhiteSpace(IMPACT_23.Text) ||
+                                String.IsNullOrWhiteSpace(IMPACT_25.Text) ||
+                                String.IsNullOrWhiteSpace(IMPACT_27.Text))
+                            {
+                                System.Windows.MessageBox.Show("You must supply all information for 3 vendors if products aren't from an approved source." +
+                                    "\n" + "\n" +
+                                    "You must also include a Description of the purchase being requested and a justification.");
+                            }
+
+                        }
+                        //for debugging
+                        //System.Windows.MessageBox.Show(approvedsource);
+                        if (approvedsource == "YES")
+                        {
+                            if (String.IsNullOrWhiteSpace(IMPACT_05.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_13.Text) ||
+                                !String.IsNullOrWhiteSpace(IMPACT_21.Text))
+                            {
+                                System.Windows.MessageBox.Show("Approved must be in the correct column");
+                            }
+                            if (String.IsNullOrWhiteSpace(IMPACT_01.Text) ||
+                                String.IsNullOrWhiteSpace(IMPACT_03.Text) ||
+                                String.IsNullOrWhiteSpace(IMPACT_05.Text) ||
+                                String.IsNullOrWhiteSpace(IMPACT_07.Text) ||
+                                String.IsNullOrWhiteSpace(IMPACT_09.Text) ||
+                                String.IsNullOrWhiteSpace(IMPACT_11.Text))
+                            {
+                                System.Windows.MessageBox.Show("You have not entered enough information for the approved vendor" + "\n" + "Please check the form and resubmit.");
+                            }
+
+                            if (!String.IsNullOrWhiteSpace(IMPACT_01.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_03.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_05.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_07.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_09.Text) &&
+                                !String.IsNullOrWhiteSpace(IMPACT_11.Text))
+                            {
+                                conDataBase.Open();
+                                myReader = cmdDataBase.ExecuteReader();
+                                System.Windows.MessageBox.Show("Purchase Request Submitted!");
+                                while (myReader.Read())
+                                {
+                                }
+                            }
                         }
                     }
                 }
-                System.Windows.MessageBox.Show("debug4");
-                conDataBase.Open();
-                            myReader = cmdDataBase.ExecuteReader();
-                            System.Windows.MessageBox.Show("Purchase Request Submitted!");
-                            while (myReader.Read())
-                            {
-                            }
             }
             catch (Exception ex)
             {
                 System.Windows.MessageBox.Show(ex.Message);
             }
         }
+
 
     }
 }
